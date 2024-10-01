c82156bf56e5c8b69c7fcaa44dfaba0b17a46db3
Andrew <andrew.segovia4@gmail.com>
2024-09-30 17:46:17 -0700

    ``

---
+++ backend/demo.springboot.firebase/src/main/java/com/example/firebase/demo/springboot/firebase/Application.java
@@ -0,0 +1,13 @@
+package com.example.firebase.demo.springboot.firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
