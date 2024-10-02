d348b0c5fbb4a8ce9e9af59fc01ffe5babe7aa87
Andrew <andrew.segovia4@gmail.com>
2024-10-01 19:51:51 -0700

    SpringBoot - Firebase Demo finalized
    Implemented
    -Create
    -Read
    -Update
    -Delete
    
    Added method to get all products

--- backend/springbootfirebase/src/main/java/com/backend/springbootfirebase/controller/ProductController.java
+++ backend/springbootfirebase/src/main/java/com/backend/springbootfirebase/controller/ProductController.java
@@ -1,12 +1,10 @@
 package com.backend.springbootfirebase.controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.backend.springbootfirebase.entity.Product;
 import com.backend.springbootfirebase.service.ProductService;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 
@@ -17,8 +15,29 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping("/products")  // Fixed the closing quote
-    public String saveProduct(@RequestBody Product product) throws ExecutionException, InterruptedException {  // Corrected the Exception name
-
-        return productService.saveProduct(product);  // Call to the service layer to save the product
+        public String saveProduct(@RequestBody Product product) throws ExecutionException, InterruptedException {
+        return productService.saveProduct(product);
     }
+
+    @GetMapping ("/products/{name}")  // Fixed the closing quote
+    public Product getProduct(@PathVariable String name ) throws ExecutionException, InterruptedException {
+        return productService.getProductDetails(name);
+        }
+
+    @GetMapping ("/products")  // Fixed the closing quote
+    public List<Product> getAllProduct() throws ExecutionException, InterruptedException {
+        return productService.getAllProductDetails();
+        }
+
+    @PutMapping ("/products")  // Fixed the closing quote
+    public String updateProduct(@RequestBody Product product) throws ExecutionException, InterruptedException {
+        return productService.updateProduct(product);
+    }
+
+    @DeleteMapping ("/products/{name}")  // Fixed the closing quote
+    public String deleteProduct(@PathVariable String name ) throws ExecutionException, InterruptedException {
+        return productService.deleteProduct(name);
+    }
+
+
 }
--- backend/springbootfirebase/src/main/java/com/backend/springbootfirebase/firebaseConnection/FirebaseInitialization.java
+++ backend/springbootfirebase/src/main/java/com/backend/springbootfirebase/firebaseConnection/FirebaseInitialization.java
@@ -3,6 +3,7 @@ package com.backend.springbootfirebase.firebaseConnection;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
@@ -10,11 +11,12 @@ import java.io.FileInputStream;
 @Service
 public class FirebaseInitialization {
 
+    @PostConstruct
     public void initialization() {
         FileInputStream serviceAccount = null;
         try {
-            serviceAccount = new FileInputStream("./serviceAccountKey.json");  // Assign the file input stream
-
+            serviceAccount = new FileInputStream("springbootfirebase/src/main/java/com/backend/springbootfirebase/serviceAccountKey.json");  // Assign the file input stream
+            System.out.println("We are here12");
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
