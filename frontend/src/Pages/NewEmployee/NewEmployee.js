f964aaaf1e94d22c63a15f29afe4f00aaaa9953b
Andrew <andrew.segovia4@gmail.com>
2024-10-04 21:19:00 -0700

    Added Neww Employee Page -Form to add employee data to db

--- frontend/src/Pages/NewEmployee/NewEmployee.js
+++ frontend/src/Pages/NewEmployee/NewEmployee.js
@@ -1,8 +1,53 @@
 import { useState } from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button"
 
 const NewEmployee = () => {
-    return(
 
+    const [formData, setFormData] = useState({
+        name: '',
+        number: ''   
+    })
+
+const handleFormChange =(e) => {
+    const (name, number) = e.target;
+    setFormData{(
+        ...formData,
+        [name]: value,
+    )}
+} 
+
+    return(
+    <>
+        <div className="center-form">
+            <h1>Add New Employee</h1>
+                <Form>
+                    <Form.Group controlId="formBasicName">
+                        <Form.Control 
+                        type="text"
+                        name="name"
+                        placeholder="Name of Employee"
+                        value={formData.name}
+                        onChange={handleFormChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicName">
+                        <Form.Control 
+                        type="text"
+                        name="number"
+                        placeholder="Name of Employee"
+                        value={formData.name}
+                        onChange={handleFormChange}
+                        />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" className="w-100">
+                        Save
+                    </Button>
+                </Form>
+        </div>
+    </>
     )
 }
 
